89abf5e116c4c084a48cbcdfd32e1150f13809a9
WALUKA <128817870+Walukapahantt@users.noreply.github.com>
2024-06-17 21:10:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+from aiogram import Bot, Dispatcher, executor, types
 import os
-
 from keep_alive import keep_alive
-
 keep_alive()
 
 os.system('code-server --auth password --bind-addr 0.0.0.0:8000 /home/runner/VSCode/')
